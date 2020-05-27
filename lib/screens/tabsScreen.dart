3abc3360544447454b84be264cf5705a975e601e
Vansh Goel <vanshg395@gmail.com>
2020-05-27 21:00:06 +0530

    ab final

--- lib/screens/tabsScreen.dart
+++ lib/screens/tabsScreen.dart
@@ -64,7 +64,7 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.white,
       body: _isLoading
           ? Center(
               child: CircularProgressIndicator(
--- lib/widgets/subcategory_card.dart
+++ lib/widgets/subcategory_card.dart
@@ -13,7 +13,18 @@ class SubcategoryCard extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
         margin: EdgeInsets.symmetric(vertical: 5),
-        color: Theme.of(context).canvasColor,
+        decoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: BorderRadius.circular(10),
+          // boxShadow: [
+          //   BoxShadow(
+          //     color: Colors.grey,
+          //     blurRadius: 2,
+          //     spreadRadius: 0.1,
+          //     offset: Offset(1, 1),
+          //   ),
+          // ],
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
